f498593f0d33a4b1fd533422e6d0f883a1657a87
Viktar Rusakovich <mrisviz.rus@gmail.com>
2024-03-01 01:00:14 +0300

    feat: add gradle

---
+++ src/main/java/by/clevertec/aspectlogger/LoggerControllerSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package by.clevertec.aspectlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerControllerSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggerControllerSpringBootStarterApplication.class, args);
+    }
+
+}
