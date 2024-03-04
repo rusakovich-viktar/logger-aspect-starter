f52760992f69a9116f0c3f30a551ac6206b377ec
Viktar Rusakovich <mrisviz.rus@gmail.com>
2024-03-04 14:12:41 +0300

    fix: fix literls add dockerfile

---
+++ src/main/java/by/clevertec/aspectlogger/util/Constant.java
@@ -0,0 +1,10 @@
+package by.clevertec.aspectlogger.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constant {
+
+    public static final String EXITING_METHOD_WITH_RESULT = "Exiting method: {} with result: {}";
+    public static final String RECEIVED_CALL_HTTP_METHOD_WITH_PATH = "Received call :: HTTP-method [{}] with path [{}]";
+}
